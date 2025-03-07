360d3584b729ecda4ddb44f5d833f3bc426c5c74
beroy <benjamin.roy69@gmail.com>
2025-03-07 10:40:08 +0100

    Ball is moving

--- public/client.js
+++ public/client.js
@@ -1,15 +1,39 @@
-var canvas = document.getElementById("gameCanvas");
-var ctx = canvas.getContext("2d");
-function draw() {
-    ctx.fillStyle = "black"; // Fond noir (si non transparent)
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "white"; // Dessiner la balle
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 10, 0, Math.PI * 2);
-    ctx.fill();
+let canvas = document.getElementById("gameCanvas");
+let ctx = canvas.getContext("2d");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-draw();
-var socket = new WebSocket("ws://localhost:8080");
+
+async function animateBall() {
+    let i = 0;
+    let dir = true;
+    while (true) {
+        let x = canvas.width * (i / 100);
+        let y = canvas.height / 2;
+
+        ctx.fillStyle = "black"; // Fond noir
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "cyan"; // Dessiner la balle
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fill();
+        await sleep(20);
+
+        if (dir === true)
+            i++;
+        else
+            i--;
+        if (i === 100)
+            dir = false;
+        else if (i === 0)
+            dir = true;
+    }
+}
+
+animateBall();
+let socket = new WebSocket("ws://localhost:8080");
 socket.onopen = function () { return console.log("Connected to server"); };
 socket.onmessage = function (event) { return console.log("Message from server: ".concat(event.data)); };
 socket.onclose = function () { return console.log("Disconnected"); };
