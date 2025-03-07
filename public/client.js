e49a5b3065cd1d40787da223e4312dffef09a8f3
beroy <benjamin.roy69@gmail.com>
2025-03-07 11:08:50 +0100

    Ball is moving

--- public/client.js
+++ public/client.js
@@ -1,35 +1,30 @@
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+//function nextPos()
 
-async function animateBall() {
-    let i = 0;
-    let dir = true;
-    while (true) {
-        let x = canvas.width * (i / 100);
-        let y = canvas.height / 2;
+let i = 0;
+let dir = true;
+let speed = 1;
 
-        ctx.fillStyle = "black"; // Fond noir
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        ctx.fillStyle = "cyan"; // Dessiner la balle
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fill();
-        await sleep(20);
-
-        if (dir === true)
-            i++;
-        else
-            i--;
-        if (i === 100)
-            dir = false;
-        else if (i === 0)
-            dir = true;
-    }
+function animateBall() {
+    let pos = {x: canvas.width * (i / 100), y: canvas.height / 2};
+    // Remplir le fond
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Dessiner la balle
+    ctx.fillStyle = "cyan";
+    ctx.beginPath();
+    ctx.arc(pos.x, pos.y, 10, 0, Math.PI * 2);
+    ctx.fill();
+    // Mise à jour de la position
+    if (dir) i += speed;
+    else i-= speed;
+    // Inversion de la direction
+    if (i >= 100) dir = false;
+    else if (i <= 0) dir = true;
+    // Relance l'animation à chaque frame
+    requestAnimationFrame(animateBall);
 }
 
 animateBall();
